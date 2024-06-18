adff52971d1baa868d3b30b10841d2e29369dc7a
DESKTOP-HQJALJL\verem <mvv@vt2b.ru>
2024-06-18 13:59:08 +0500

    feat: Обновил описание + скилы

--- src/tsx-components/main/right-blok/about_window/about_window.tsx
+++ src/tsx-components/main/right-blok/about_window/about_window.tsx
@@ -31,12 +31,15 @@ const AboutWindow = ({nav}: any) => {
                к образовательному процессу.
             </p>
             <p>
+               Я быстро погружаюсь в новые проекты и осваиваю базовые принципы новых инструментов. Для Flutter мне понадобилось 2 недели, чтобы начать выполнять коммерческие задачи, а для Vue 3 – 1 неделя для разработки проекта. Этих базовых знаний хватило для успешной работы, и в дальнейшем обучении серьезных проблем не возникало.
+            </p>
+            <p>
                На данный момент работаю над full-stack проектом для своего <a href="https://discord.gg/xcrfrJ5QsN" className={'hover_a'}>дискорд канала</a> посвещенного игре
                <a onMouseEnter={handelHover}
                   onMouseLeave={handelLeave}
                   className={`${styles.hover_osu} ${stylesOsu}`}
                   href="https://osu.ppy.sh/">
-               <span className={styles.span_osu_1}>O</span><span className={styles.span_osu_2}>s</span><span className={styles.span_osu_3}>u</span><span className={styles.span_osu_4}>!</span></a>
+               <span className={styles.span_osu_1}>O</span><span className={styles.span_osu_2}>s</span><span className={styles.span_osu_3}>u</span><span className={styles.span_osu_4}>!</span></a>. <br/>Планирую переписать старый сайт с использованием React и Bootstrap на React Next и Tailwind CSS для более глубокого изучения этих инструментов.
             </p>
          </div>
       </section>
--- src/tsx-components/main/right-blok/skills_window/skills_window.tsx
+++ src/tsx-components/main/right-blok/skills_window/skills_window.tsx
@@ -4,38 +4,45 @@ const SkillsWindow = ({nav}: any) => {
    return (
       <section ref={nav}>
          <div className={`header_paragraph`}><h2>Навыки</h2></div> {/* Прилипает */}
-         <div className={`d-flex mw-100 overflow-x-hidden flex-wrap ${styles.skills}`}>
-            <span>JavaScript</span>
-            <span>TypeScript</span>
-            <span>C#</span>
-            <span>C++</span>
-            <span>OOP</span>
-            <span>SQL</span>
-            <span>HTML5</span>
-            <span>CSS</span>
-            <span>Sass</span>
-            <span>ReactJS</span>
-            <span>BEM</span>
-            <span>Git</span>
-            <span>Bootstrap</span>
-            <span>Mobx</span>
-            <span>WebSocket</span>
-            <span>jQuery</span>
-            <span>Node.js</span>
-            <span>PHP</span>
-            <span>Ajax</span>
-            <span>Адаптивная и
+          <div className={`d-flex mw-100 overflow-x-hidden flex-wrap ${styles.skills}`}>
+              <span>JavaScript</span>
+              <span>TypeScript</span>
+              <span>C#</span>
+              <span>C++</span>
+              <span>OOP</span>
+              <span>SQL</span>
+              <span>HTML5</span>
+              <span>CSS</span>
+              <span>Sass</span>
+              <span>ReactJS</span>
+              <span>React Next</span>
+              <span>React Native</span>
+              <span>Flutter</span>
+              <span>Vue 2</span>
+              <span>Vue 3</span>
+              <span>Refine</span>
+              <span>Tailwind CSS</span>
+              <span>BEM</span>
+              <span>Git</span>
+              <span>Bootstrap</span>
+              <span>Mobx</span>
+              <span>WebSocket</span>
+              <span>jQuery</span>
+              <span>Node.js</span>
+              <span>PHP</span>
+              <span>Ajax</span>
+              <span>Адаптивная и
 отзывчивая верстка</span>
-            <span>API</span>
-            <span>Ubuntu</span>
-            <span>Backend</span>
-            <span>Material Ui</span>
-            <span>Framer Motion</span>
-            <span>React
+              <span>API</span>
+              <span>Ubuntu</span>
+              <span>Backend</span>
+              <span>Material Ui</span>
+              <span>Framer Motion</span>
+              <span>React
 Router Dom</span>
-            <span>Figma</span>
-            <span>Photoshop</span>
-         </div>
+              <span>Figma</span>
+              <span>Photoshop</span>
+          </div>
       </section>
    );
 };
